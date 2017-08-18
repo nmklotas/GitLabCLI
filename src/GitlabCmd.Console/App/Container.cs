a4330e7204fb7def83ebab8369caaf85d31db24a
nmklotas <nemoku@gmail.com>
2017-08-19 00:32:06 +0300

    add default help screen for parser

--- src/GitlabCmd.Console/App/Container.cs
+++ src/GitlabCmd.Console/App/Container.cs
@@ -1,11 +1,9 @@
-using System;
 using Castle.MicroKernel.Registration;
 using Castle.Windsor;
 using CommandLine;
 using GitlabCmd.Console.Configuration;
 using GitlabCmd.Console.GitLab;
 using Microsoft.Extensions.Configuration;
-using NGitLab;
 
 namespace GitlabCmd.Console.App
 {
@@ -14,11 +12,12 @@ namespace GitlabCmd.Console.App
         public static WindsorContainer Build()
         {
             var container = new WindsorContainer();
-            container.Register(Component.For<Parser>());
+            container.Register(Component.For<Parser>().UsingFactoryMethod(c => Parser.Default));
             container.Register(Component.For<GitLabFacade>());
             container.Register(Component.For<LaunchHandler>());
             container.Register(Component.For<ParametersHandler>());
             container.Register(Component.For<OutputPresenter>());
+            container.Register(Component.For<ConfigurationHandler>());
             container.Register(Component.For<GitLabIssueHandler>());
             container.Register(Component.For<MergeRequestsHandler>());
             container.Register(Component.For<AppSettingsValidationHandler>());
