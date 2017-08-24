23e5c8e745c173c5970d78a1897eec341e85528a
nmklotas <nemoku@gmail.com>
2017-08-24 13:02:22 +0300

    Remove not used methods

--- src/GitlabCmd.Core/Result.cs
+++ src/GitlabCmd.Core/Result.cs
@@ -1,6 +1,5 @@
 using System;
 using System.Diagnostics;
-using System.Linq;
 
 namespace GitLabCmd.Core
 {
@@ -71,53 +70,6 @@ namespace GitLabCmd.Core
 
         [DebuggerStepThrough]
         public static Result<T> Fail<T>(Result result) => new Result<T>(true, default, result.Error);
-
-        /// <summary>
-        /// Returns first failure in the list of <paramref name="results"/>. If there is no failure returns success.
-        /// </summary>
-        /// <param name="results">List of results.</param>
-        [DebuggerStepThrough]
-        public static Result FirstFailureOrSuccess(params Result[] results)
-        {
-            foreach (Result result in results)
-            {
-                if (result.IsFailure)
-                    return Fail(result.Error);
-            }
-
-            return Ok();
-        }
-
-        /// <summary>
-        /// Returns failure which combined from all failures in the <paramref name="results"/> list. Error messages are separated by <paramref name="errorMessagesSeparator"/>. 
-        /// If there is no failure returns success.
-        /// </summary>
-        /// <param name="errorMessagesSeparator">Separator for error messages.</param>
-        /// <param name="results">List of results.</param>
-        [DebuggerStepThrough]
-        public static Result Combine(string errorMessagesSeparator, params Result[] results)
-        {
-            var failedResults = results.Where(x => x.IsFailure).ToList();
-
-            if (!failedResults.Any())
-                return Ok();
-
-            string errorMessage = string.Join(errorMessagesSeparator, failedResults.Select(x => x.Error).ToArray());
-            return Fail(errorMessage);
-        }
-
-        [DebuggerStepThrough]
-        public static Result Combine(params Result[] results) => Combine(", ", results);
-
-        [DebuggerStepThrough]
-        public static Result Combine<T>(params Result<T>[] results) => Combine(", ", results);
-
-        [DebuggerStepThrough]
-        public static Result Combine<T>(string errorMessagesSeparator, params Result<T>[] results)
-        {
-            Result[] untyped = results.Select(result => (Result)result).ToArray();
-            return Combine(errorMessagesSeparator, untyped);
-        }
     }
 
     public struct Result<T>
