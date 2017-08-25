41c6151af4734b02fef627d1658a03cd1b65b0a1
nmklotas <nemoku@gmail.com>
2017-08-25 17:32:08 +0300

    Fix exit 0

--- test.ps1
+++ test.ps1
@@ -1,2 +1,14 @@
-dotnet test
-exit 0
+dotnet test test\GitlabCmd.Console.Test | Write-Host
+if ($LastExitCode -ne 0)
+{
+	exit 1
+}
+
+dotnet test test\GitlabCmd.Gitlab.Test | Write-Host
+if ($LastExitCode -ne 0)
+{
+	exit 1
+}
+
+
+
