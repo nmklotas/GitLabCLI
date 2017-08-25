7494e398b890d33bed531f416fb0cfb36794447f
nmklotas <nemoku@gmail.com>
2017-08-25 14:14:26 +0300

    Go back

--- test.ps1
+++ test.ps1
@@ -1,11 +1,2 @@
-dotnet test test\GitlabCmd.Console.Test | Write-Host
-if ($LastExitCode -ne 0)
-{
-	exit 1
-}
-
-dotnet test test\GitlabCmd.Gitlab.Test | Write-Host
-if ($LastExitCode -ne 0)
-{
-	exit 1
-}
+dotnet test
+exit 0
